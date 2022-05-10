116faa6c4ac6c642ca61a168654e0c5b1889788f
GET Academy <terje@getacademy.no>
2022-05-10 11:18:33 +0200

    Initial commit

---
+++ Modul3Intro202205/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Modul3Intro202205
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //static int Main(string[] args)
+        //{
+        //    Console.WriteLine($"args sin lengde er {args.Length}");
+        //    for (int i = 0; i < args.Length; i++)
+        //    {
+        //        Console.WriteLine($"Indeks {i} => {args[i]}");
+        //    }
+
+        //    Console.WriteLine(GetTerje());
+
+        //    return 5;
+        //}
+
+        //static string GetTerje()
+        //{
+        //    return "Terje!";
+        //}
+
+        //static void GetTerje2()
+        //{
+        //    Console.WriteLine("Terje!");
+        //}
+    }
+}
